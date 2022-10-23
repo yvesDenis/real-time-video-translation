8c980e8d00b6bdb2cd4e624e1732dc140c4c4fe3
DEFFO DENIS <yvesdeffo@gmail.com>
2022-10-22 22:28:05 -0400

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import http from 'http'
+import { app } from './app'
+
+const port = 3000
+app.set('port', port)
+
+const errorHandler = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+  const address = server.address()
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
+
+const server = http.createServer(app)
+
+server.on('error', errorHandler)
+server.on('listening', () => {
+  const address = server.address()
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port
+  console.log('Listening on ' + bind)
+})
+
+server.listen(port)
